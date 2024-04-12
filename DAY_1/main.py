acb98b9ecee4dc43ec920f873511ebe7d9134682
vitoriahelens <vitoria.devops@outlook.com>
2024-04-11 21:19:36 -0300

    add DAY_1

---
+++ DAY_1/main.py
@@ -0,0 +1,47 @@
+# print("Hello World!")
+
+# ## PRINT
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to ride for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.")
+# print("A 'single quote' inside a double quote")
+# print('A "double quote" inside a single quote')
+# #a barra de 'escape' diz ao computador que ignore aquele trecho como código
+# print("Alternatively you can just \"escape\" the quote")
+
+# ## STRING MANIPULATION
+# print("Hello World!\nHello World!")
+# print("Hello" + " " + "Vitoria")
+
+# ## INPUT FUNCTION
+# print("Hello "+ input("What is your name?"))
+# print ("Insert a number: ")
+# num1 = int(input())
+# print ("Insert another number: ")
+# num2 = int(input())
+# print ("Result: ")
+# print(num1 * num2)
+# print(len(input()))
+# print( len( input("What is your name?") ) )
+
+## VARIABLES
+# name = input("What is your name? ")
+# lenght = len(name)
+# print(lenght)
+## NAMING VARIABLES
+# user_name
+# username
+# username1
+
+### FINAL PROJECT: BAND NAME GENERATOR
+print("Welcome to the band name generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+
+
